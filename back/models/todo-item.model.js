c73eb8d6557869d3d22e83ecb71748dc0c08e433
David_Gavilanes <davidgavilanesvelez@gmail.com>
2020-08-21 12:37:20 +0200

    added model to back

---
+++ back/models/todo-item.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const todoItemSchema = new Schema(
+  {
+    description: { type: String, required: true },
+    isDone: { type: Boolean, required: true },
+  },
+  { timestamps: true }
+);
+
+const TodoItem = mongoose.model("TodoItem", todoItemSchema);
+
+module.exports = TodoItem;
